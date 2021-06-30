89b08eb7ca8dea6f44ce545b47aeb428e1330e32
Alfian <muhamad.alfian@outlook.com>
2021-07-01 05:41:26 +0700

    Reorganize build workflow

--- webpack.deploy.js
+++ webpack.deploy.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies */
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
